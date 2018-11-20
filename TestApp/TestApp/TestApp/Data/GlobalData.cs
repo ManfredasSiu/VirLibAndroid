8e9f3480f23c4c9e12b3a6669cc15249d86a4dfc
ManfredasSiu <43031678+ManfredasSiu@users.noreply.github.com>
2018-11-20 17:15:52 +0200

    sorting files

---
+++ TestApp/TestApp/TestApp/Data/GlobalData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.Data
+{
+    public class GlobalData
+    {
+        UserData CurrentUser;
+        List<Book> allBooks = new List<Book>(); 
+    }
+}
--- TestApp/TestApp/TestApp/Data/UserData.cs
+++ TestApp/TestApp/TestApp/Data/UserData.cs
@@ -4,8 +4,12 @@ using System.Text;
 
 namespace TestApp
 {
-    public class UserData : User
+    public class UserData
     {
+        public int UserID;
+        public string UserName;
+        public string UserStatus;
+        public string UserEmail;
         List<Book> UserBooks = new List<Book>();
         List<Book> BooksRead = new List<Book>();
     }
